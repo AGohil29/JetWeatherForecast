3d8ab503498174b24750a9f1911f3aa7d9462139
ArunGoldmedal <104761830+ArunGoldmedal@users.noreply.github.com>
2023-05-27 18:37:04 +0530

    Splash UI

--- app/src/main/java/com/goldmedal/jetweatherforecast/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/goldmedal/jetweatherforecast/screens/WeatherSplashScreen.kt
@@ -1,10 +1,57 @@
 package com.goldmedal.jetweatherforecast.screens
 
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.goldmedal.jetweatherforecast.R
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
-    Text(text = "Splash Screen")
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(320.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun),
+                contentDescription = "sunny icon",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(90.dp)
+            )
+            Text(text = "Find the Sun?",
+            style = MaterialTheme.typography.h5,
+            color = Color.LightGray)
+        }
+
+    }
 }
