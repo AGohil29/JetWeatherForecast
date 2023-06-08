0c4636a0328140b0f8c728a32a7be3a278e4d867
ArunGoldmedal <104761830+ArunGoldmedal@users.noreply.github.com>
2023-06-08 12:16:28 +0530

    Setting up AppBar - Part 1.

--- app/src/main/java/com/goldmedal/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/goldmedal/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.goldmedal.jetweatherforecast.screens.main
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
@@ -8,15 +10,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.goldmedal.jetweatherforecast.data.DataOrException
 import com.goldmedal.jetweatherforecast.model.Weather
-import com.goldmedal.jetweatherforecast.model.WeatherItem
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
-    ShowData(mainViewModel)
-}
-
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)
     ) {
@@ -26,8 +22,22 @@ fun ShowData(mainViewModel: MainViewModel) {
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text(text = "Main Screen ${weatherData.data!!.city.country}")
+        MainScaffold(weatherData.data!!, navController)
+    }
+}
+
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+
+    Scaffold(topBar = {
+        WeatherAppBar(title = "Mumbai, IN")
+    }) {
+        MainContent(weather, it)
     }
 
+}
 
+@Composable
+fun MainContent(weather: Weather, paddingValues: PaddingValues) {
+    Text(text = weather.city.name)
 }
